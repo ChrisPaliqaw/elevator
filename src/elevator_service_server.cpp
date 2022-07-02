393323352e3a43f1f46023f215c2249c7e14a66d
Chris Paliqaw <christophomos@gmail.com>
2022-07-02 14:18:38 +0000

    Add elevator_up publisher but don't yet use

--- src/elevator_service_server.cpp
+++ src/elevator_service_server.cpp
@@ -3,10 +3,15 @@
 #include "phase2_custom_interfaces/srv/detail/elevator__struct.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "phase2_custom_interfaces/srv/elevator.hpp"
+#include "std_msgs/msg/empty.hpp"
 
 using phase2_custom_interfaces::srv::Elevator;
-// using Empty = std_srvs::srv::Empty;
+
 rclcpp::Node::SharedPtr g_node = nullptr;
+
+rclcpp::WallRate g_loop_rate(2);
+rclcpp::Publisher<std_msgs::msg::Empty>::SharedPtr g_up_publisher;
+std::shared_ptr<std_msgs::msg::Empty> g_message;
 
 void my_handle_service(
     const std::shared_ptr<rmw_request_id_t> request_header,
@@ -22,6 +27,11 @@ int main(int argc, char ** argv)
     rclcpp::init(argc, argv);
     g_node = rclcpp::Node::make_shared("elevator_service_server");
     auto server = g_node->create_service<Elevator>("elevator", my_handle_service);
+
+    g_up_publisher =
+      g_node->create_publisher<std_msgs::msg::Empty>("elevator_up", 10);
+    g_message = std::make_shared<std_msgs::msg::Empty>();
+
     rclcpp::spin(g_node);
     rclcpp::shutdown();
     g_node = nullptr;
